fa0af05a8a8119ed19ea37d242213a5d1587a829
reda <hissocka2006@gmail.com>
2025-03-12 22:59:36 +0100

    done with the first part

--- Learning_log/learning_logs/urls.py
+++ Learning_log/learning_logs/urls.py
@@ -5,6 +5,9 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
-    #Home page
+    # Home page
     path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+
 ]
